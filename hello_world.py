c98e6db5ee063d770185d6eece05ea24cc58243f
amizzuddin <amizzuddinamin@outlook.com>
2023-01-13 19:31:26 +0800

    test pre-commit 2

--- hello_world.py
+++ hello_world.py
@@ -5,9 +5,10 @@ class person:
     name: str
     age: int
     company: str
+    nationality: str
 
 if __name__ == "__main__":
     print("Hello World!")
 
-    first_person = person(name="Amizzuddin", age=35, company="Conti")
+    first_person = person(name="Amizzuddin", age=35, company="Conti", nationality="malaysian")
     print(first_person)
