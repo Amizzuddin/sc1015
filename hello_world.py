91ed847e5209d13c9a7418ca6e785889271703ac
amizzuddin <amizzuddinamin@outlook.com>
2023-01-13 19:21:37 +0800

    test pre-commit

--- hello_world.py
+++ hello_world.py
@@ -4,9 +4,10 @@ from dataclasses import dataclass
 class person:
     name: str
     age: int
+    company: str
 
 if __name__ == "__main__":
     print("Hello World!")
 
-    first_person = person(name="Amizzuddin", age=35)
+    first_person = person(name="Amizzuddin", age=35, company="Conti")
     print(first_person)
