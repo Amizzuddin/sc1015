80a3f2430e664a81ef016d5d78bf174f9044d280
amizzuddin <amizzuddinamin@outlook.com>
2023-01-13 19:16:39 +0800

    test mypy 1

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass 
+class person:
+    name: str
+    age: int
+
 if __name__ == "__main__":
     print("Hello World!")
+
+    first_person = person(name="Amizzuddin", age=35)
+    print(first_person)
